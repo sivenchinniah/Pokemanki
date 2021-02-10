d09784b2188d67a6f1947b44eabe171e441bde01
bluegreenmagick <bluegreenmagick@gmail.com>
2021-02-10 14:39:25 +0900

    show warning on second profile open

--- __init__.py
+++ __init__.py
@@ -3,9 +3,24 @@
 # JK please feel free to use this however you would like.
 
 from aqt import gui_hooks
+from aqt.utils import showWarning
+
+
+opened = False
 
 
 def startup():
+    global opened
+    if opened:
+        warning_text = "\n".join((
+            "Pokemanki does not support opening a second profile in one session.",
+            "Please close Anki and reopen it again to the desired profile.",
+            "Pokemanki may behave strangely"
+        ))
+        showWarning(warning_text, title="Pokemanki won't function properly")
+        return
+
+    opened = True
     from . import main
 
 
