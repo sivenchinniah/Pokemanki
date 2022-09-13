05889c0871c46b4e4d107cf1a8b947263184cb54
Exkywor <fernandoquant.dev@gmail.com>
2022-09-13 15:16:39 -0600

    Add number of Pokéemon. Fix Pokemon typo

--- display.py
+++ display.py
@@ -14,25 +14,25 @@ def pokemon_display(istagmon, wholecollection=True):
     Control the generation of the html code to display.
 
     :param bool istagmon: True to switch to use tag's displau, False for deck.
-    :param bool wholecollection: True if multiple Pokemon, false if single.
+    :param bool wholecollection: True if multiple Pokémon, false if single.
     :return: The html text to display.
     :rtype: str
     """
 
-    # Assign Pokemon Image and Progress Bar folder directory names
+    # Assign Pokémon Image and Progress Bar folder directory names
     pkmnimgfolder = currentdirname / "pokemon_images"
     progressbarfolder = currentdirname / "progress_bars"
 
-    # Move Pokemon Image folder to collection.media folder if not already there
+    # Move Pokémon Image folder to collection.media folder if not already there
     # (Anki reads from here when running anki.stats.py)
     if (not os.path.exists(f"{mediafolder}/pokemon_images")) and os.path.exists(pkmnimgfolder):
         shutil.copytree(pkmnimgfolder, f"{mediafolder}/pokemon_images")
     if (not os.path.exists(f"{mediafolder}/progress_bars")) and os.path.exists(progressbarfolder):
         shutil.copytree(progressbarfolder, f"{mediafolder}/progress_bars")
 
-    # Get list of Pokemon from tags or decks.
-    #   For decks, if wholeCollection, get all assigned Pokemon and assign to Pokemon,
-    #   else, show Pokemon for either single deck or all subdecks and store in Pokemon
+    # Get list of Pokémon from tags or decks.
+    #   For decks, if wholeCollection, get all assigned Pokémon and assign to Pokémon,
+    #   else, show Pokémon for either single deck or all subdecks and store in Pokémon
     pokemon = TagPokemon() if istagmon else MultiPokemon(wholecollection)
 
     result = _show(pokemon)
@@ -44,7 +44,7 @@ def _show(data):
     """
     Generate the html to inject into the new stats window.
 
-    :param data: Pokemon information. Tuple if single Pokemon, list if collection.
+    :param data: Pokémon information. Tuple if single Pokémon, list if collection.
     :return: The html code to display.
     :rtype: str
     """
@@ -53,14 +53,14 @@ def _show(data):
         return ""
 
     # Pokemanki container header
-    txt = '<h1 style="text-align: center; font-weight: 700; margin-top: 40px;">Pokemanki</h1>' \
-          '<div style="text-align: center;">Your pokemon</div>'
+    txt = '<h1 style="text-align: center; font-weight: 700; margin-top: 40px;">Pokémon</h1>' \
+          '<div style="text-align: center;">Your Pokémon</div>'
 
-    # If single Pokemon, show centered card
+    # If single Pokémon, show centered card
     if type(data) == tuple:
         txt += '<div class="pk-st-single">'
         txt += _card_html(data[0], data[1], data[2], data[3] if len(data) == 4 else "")
-    # If multiple Pokemon, show flex row of cards
+    # If multiple Pokémon, show flex row of cards
     elif type(data) == list:
         if len(data) == 1:
             txt += '<div class="pk-st-single">'
@@ -69,12 +69,15 @@ def _show(data):
             txt += '<div class="pk-st-container">'
             multi = True
 
-        _num_pokemon = len(data)
         sortedData = sorted(data, key=lambda k: k[2], reverse=True)
         for pokemon in sortedData:
             txt += _card_html(pokemon[0], pokemon[1], pokemon[2], pokemon[3] if len(pokemon) == 4 else "", multi)
 
+    # Close cards container
     txt += '</div>'
+
+    # Pokémon total
+    txt += f'<h4 style="text-align: center; margin-top: 5px;"><b>Total:</b> {len(data)} Pokémon</h4>'
 
     # Return txt
     return txt
@@ -82,13 +85,13 @@ def _show(data):
 
 def _card_html(name, source, level, nickname="", multi=False):
     """
-    Generate the html text for a Pokemon card.
+    Generate the html text for a Pokémon card.
 
-    :param str name: Name of the Pokemon.
-    :param source: Id of the deck or name of the tqg the Pokemon belongs to.
-    :param int level: The Pokemon's lvl.
-    :param str nickname: Pokemon's nickname, if it has any.
-    :param bool multi: True if multiple Pokemon are being rendered.
+    :param str name: Name of the Pokémon.
+    :param source: Id of the deck or name of the tqg the Pokémon belongs to.
+    :param int level: The Pokémon's lvl.
+    :param str nickname: Pokémon's nickname, if it has any.
+    :param bool multi: True if multiple Pokémon are being rendered.
     :return: The card html.
     :rtype: str
     """
@@ -144,7 +147,7 @@ def _card_html(name, source, level, nickname="", multi=False):
     # End card
     card += '</div>'
 
-    # TODO: Add # of Pokemon
+    # TODO: Add # of Pokémon
     # Make bottom line using function from stats.py and assign to text_lines
     # line( text_lines, "<b>Total</b>", "</b>%s Pokémon<b>" % _num_pokemon)
 
@@ -184,10 +187,10 @@ def _in_list(listname, item):
 
 def _image_name(name, source):
     """
-    Get the image name based on the Pokemon's name and any special attributes.
+    Get the image name based on the Pokémon's name and any special attributes.
 
-    :param str name: Pokemon's name.
-    :param source: Id of the deck or tag name the Pokemon belongs to.
+    :param str name: Pokémon's name.
+    :param source: Id of the deck or tag name the Pokémon belongs to.
     :return: The image name to be used to retrieve it.
     :rtype: str
     """
@@ -242,9 +245,9 @@ def _calculate_xp_progress(level):
 
 def _held_html(source):
     """
-    Generate the held html code for the given Pokemon.
+    Generate the held html code for the given Pokémon.
 
-    :param source: Id of the deck or tag name the Pokemon belongs to.
+    :param source: Id of the deck or tag name the Pokémon belongs to.
     :return: The concatenation of all held items' html. Empty if it has no items.
     :rtype: str
     """
