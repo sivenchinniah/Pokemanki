c684d78ab95ddaa677094ecf8d9d27fc1022f73c
Exkywor <fernandoquant.dev@gmail.com>
2022-09-13 15:47:16 -0600

    Refactor repetition of trades code

--- trades.py
+++ trades.py
@@ -158,7 +158,7 @@ class Trades:
                 return
         possiblehaveslist = []
         while i < 3:
-            randno = random.randint(0, len(deckmonlist)-1)
+            randno = random.randint(0, len(deckmonlist) - 1)
             pokemon = deckmonlist[randno]
             for item in self.allpokemon:
                 if pokemon[0] == item[0]:
@@ -189,7 +189,7 @@ class Trades:
                     if (item[1] == "B" or item[1] == "A") and (item[2] < int(pokemon[2]) < item[3]):
                         possiblehaves.append(item)
             if len(possiblehaves) > 1:
-                randno = random.randint(0, len(possiblehaves)-1)
+                randno = random.randint(0, len(possiblehaves) - 1)
                 have = possiblehaves[randno]
             elif len(possiblehaves) == 1:
                 have = possiblehaves[0]
@@ -283,7 +283,11 @@ class Trades:
                    <td height = 30 width = 150 align = center><b>%s</b></td>
                    <td height = 30 width = 150 align = center><b>%s</b></td>
                    </tr>
-                   </table>""" % (self.dirname, self.trades[0][0][0], self.dirname, self.trades[1][0][0], self.dirname, self.trades[2][0][0], self.trades[0][0][0], self.trades[1][0][0], self.trades[2][0][0], self.mediafolder, self.trades[0][1][0], self.mediafolder, self.trades[1][1][0], self.mediafolder, self.trades[2][1][0], self.trades[0][1][0], self.trades[1][1][0], self.trades[2][1][0])
+                   </table>""" % (
+            self.dirname, self.trades[0][0][0], self.dirname, self.trades[1][0][0], self.dirname, self.trades[2][0][0],
+            self.trades[0][0][0], self.trades[1][0][0], self.trades[2][0][0], self.mediafolder, self.trades[0][1][0],
+            self.mediafolder, self.trades[1][1][0], self.mediafolder, self.trades[2][1][0], self.trades[0][1][0],
+            self.trades[1][1][0], self.trades[2][1][0])
         lbl1 = QLabel(table, tradewindow)
         lbl1.move(0, 0)
         lbl2 = QLabel("", tradewindow)
@@ -300,232 +304,19 @@ class Trades:
         tradewindow.exec_()
 
     def trade1(self):
-        have = self.trades[0][0]
-        want = self.trades[0][1]
-        possiblefits = []
-        f = self.f
-        if f:
-            deckmonlist = get_json("_tagmon.json")
-            if deckmonlist:
-                sorteddeckmonlist = list(reversed(deckmonlist))
-                deckmons = []
-                for item in sorteddeckmonlist:
-                    for thing in deckmons:
-                        if item[1] == thing[1]:
-                            break
-                    else:
-                        deckmons.append(item)
-                deckmonlist = deckmons
-            else:
-                nopokemon = QMessageBox()
-                nopokemon.setWindowTitle("Pokemanki")
-                nopokemon.setText(
-                    "Please open the Stats window to get your Pokémon.")
-                nopokemon.exec_()
-                return
-        else:
-            deckmonlist = get_json("_pokemanki.json", None)
-            if deckmonlist:
-                sorteddeckmonlist = list(reversed(deckmonlist))
-                deckmons = []
-                for item in sorteddeckmonlist:
-                    for thing in deckmons:
-                        if item[1] == thing[1]:
-                            break
-                    else:
-                        deckmons.append(item)
-                deckmonlist = deckmons
-            else:
-                nopokemon = QMessageBox()
-                nopokemon.setWindowTitle("Pokemanki")
-                nopokemon.setText(
-                    "Please open the Stats window to get your Pokémon.")
-                nopokemon.exec_()
-                return
-        for item in deckmonlist:
-            if item[0] == want[0] or (item[0].startswith("Eevee") and want[0] == "Eevee") and int(item[2]) >= 5:
-                possiblefits.append(item)
-        if possiblefits == []:
-            novalidpokemon = QMessageBox()
-            novalidpokemon.setWindowTitle("Pokemanki")
-            novalidpokemon.setText(
-                "Sorry, you do not have the Pokemon needed to complete this trade.")
-            novalidpokemon.exec_()
-            return
-        displaylist = []
-        for item in possiblefits:
-            deckname = mw.col.decks.name(item[1])
-            if len(item) == 4:
-                if item[0].startswith("Eevee"):
-                    displaytext = "%s - Eevee (Level %s) from %s" % (
-                        item[3], int(item[2]), deckname)
-                else:
-                    displaytext = "%s - %s (Level %s) from %s" % (
-                        item[3], item[0], int(item[2]), deckname)
-            else:
-                if item[0].startswith("Eevee"):
-                    displaytext = "Eevee (Level %s) from %s" % (
-                        int(item[2]), deckname)
-                else:
-                    displaytext = "%s (Level %s) from %s" % (
-                        item[0], int(item[2]), deckname)
-            displaylist.append(displaytext)
-        totallist = list(zip(possiblefits, displaylist))
-        possiblepokemon = QWidget()
-        inp, ok = QInputDialog.getItem(
-            possiblepokemon, "Pokemanki", "Choose a Pokemon to trade for %s" % have[0], displaylist, 0, False)
-        tradepokemon = []
-        if ok and inp:
-            for thing in totallist:
-                if inp in thing:
-                    tradepokemon = thing[0]
-                    displaytext = inp
-        if not tradepokemon:
-            return
-        confirmation = QMessageBox()
-        confirmation.setWindowTitle("Pokemanki")
-        confirmation.setText(
-            "Are you sure you want to trade your %s for a %s" % (displaytext, have[0]))
-        confirmation.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-        confirmation.setDefaultButton(QMessageBox.No)
-        result = confirmation.exec_()
-        if result == QMessageBox.Yes:
-            modifieddeckmonlist = []
-            for item in deckmonlist:
-                if item[1] == tradepokemon[1]:
-                    if have[0] == "Eevee":
-                        eevee = random.randint(1, 3)
-                        newpokemon = ("Eevee%s" % eevee, item[1], item[2])
-                    else:
-                        newpokemon = (have[0], item[1], item[2])
-                    modifieddeckmonlist.append(newpokemon)
-                else:
-                    modifieddeckmonlist.append(item)
-            write_json("_pokemanki.json", modifieddeckmonlist)
-            self.tradewindow.done(QDialog.Accepted)
-            tradedone = QMessageBox()
-            tradedone.setWindowTitle("Pokemanki")
-            tradedone.setText("You have traded your %s for a %s" %
-                              (displaytext, have[0]))
-            tradedone.exec_()
+        self.make_trade(self.trades[0][0], self.trades[0][1])
 
     def trade2(self):
-        have = self.trades[1][0]
-        want = self.trades[1][1]
+        self.make_trade(self.trades[1][0], self.trades[1][1])
+
+    def trade3(self):
+        self.make_trade(self.trades[2][0], self.trades[2][1])
+
+    def make_trade(self, have, want):
         possiblefits = []
         f = self.f
         if f:
             deckmonlist = get_json("_tagmon.json")
-            if deckmonlist:
-                sorteddeckmonlist = list(reversed(deckmonlist))
-                deckmons = []
-                for item in sorteddeckmonlist:
-                    for thing in deckmons:
-                        if item[1] == thing[1]:
-                            break
-                    else:
-                        deckmons.append(item)
-                deckmonlist = deckmons
-            else:
-                nopokemon = QMessageBox()
-                nopokemon.setWindowTitle("Pokemanki")
-                nopokemon.setText(
-                    "Please open the Stats window to get your Pokémon.")
-                nopokemon.exec_()
-                return
-        else:
-            deckmonlist = get_json("_pokemanki.json", None)
-            if deckmonlist:
-                sorteddeckmonlist = list(reversed(deckmonlist))
-                deckmons = []
-                for item in sorteddeckmonlist:
-                    for thing in deckmons:
-                        if item[1] == thing[1]:
-                            break
-                    else:
-                        deckmons.append(item)
-                deckmonlist = deckmons
-            else:
-                nopokemon = QMessageBox()
-                nopokemon.setWindowTitle("Pokemanki")
-                nopokemon.setText(
-                    "Please open the Stats window to get your Pokémon.")
-                nopokemon.exec_()
-                return
-        for item in deckmonlist:
-            if item[0] == want[0] or (item[0].startswith("Eevee") and want[0] == "Eevee") and int(item[2]) >= 5:
-                possiblefits.append(item)
-        if possiblefits == []:
-            novalidpokemon = QMessageBox()
-            novalidpokemon.setWindowTitle("Pokemanki")
-            novalidpokemon.setText(
-                "Sorry, you do not have the Pokémon needed to complete this trade.")
-            novalidpokemon.exec_()
-            return
-        displaylist = []
-        for item in possiblefits:
-            deckname = mw.col.decks.name(item[1])
-            if len(item) == 4:
-                if item[0].startswith("Eevee"):
-                    displaytext = "%s - Eevee (Level %s) from %s" % (
-                        item[3], int(item[2]), deckname)
-                else:
-                    displaytext = "%s - %s (Level %s) from %s" % (
-                        item[3], item[0], int(item[2]), deckname)
-            else:
-                if item[0].startswith("Eevee"):
-                    displaytext = "Eevee (Level %s) from %s" % (
-                        int(item[2]), deckname)
-                else:
-                    displaytext = "%s (Level %s) from %s" % (
-                        item[0], int(item[2]), deckname)
-            displaylist.append(displaytext)
-        totallist = list(zip(possiblefits, displaylist))
-        possiblepokemon = QWidget()
-        inp, ok = QInputDialog.getItem(
-            possiblepokemon, "Pokemanki", "Choose a Pokemon to trade for %s" % have[0], displaylist, 0, False)
-        tradepokemon = []
-        if ok and inp:
-            for thing in totallist:
-                if inp in thing:
-                    tradepokemon = thing[0]
-                    displaytext = inp
-        if not tradepokemon:
-            return
-        confirmation = QMessageBox()
-        confirmation.setWindowTitle("Pokemanki")
-        confirmation.setText(
-            "Are you sure you want to trade your %s for a %s" % (displaytext, have[0]))
-        confirmation.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
-        confirmation.setDefaultButton(QMessageBox.No)
-        result = confirmation.exec_()
-        if result == QMessageBox.Yes:
-            modifieddeckmonlist = []
-            for item in deckmonlist:
-                if item[1] == tradepokemon[1]:
-                    if have[0] == "Eevee":
-                        eevee = random.randint(1, 3)
-                        newpokemon = ("Eevee%s" % eevee, item[1], item[2])
-                    else:
-                        newpokemon = (have[0], item[1], item[2])
-                    modifieddeckmonlist.append(newpokemon)
-                else:
-                    modifieddeckmonlist.append(item)
-            write_json("_pokemanki.json", modifieddeckmonlist)
-            self.tradewindow.done(QDialog.Accepted)
-            tradedone = QMessageBox()
-            tradedone.setWindowTitle("Pokemanki")
-            tradedone.setText("You have traded your %s for a %s" %
-                              (displaytext, have[0]))
-            tradedone.exec_()
-
-    def trade3(self):
-        have = self.trades[2][0]
-        want = self.trades[2][1]
-        possiblefits = []
-        f = self.f
-        if f:
-            deckmonlist = get_json("_tagmon.json", None)
             if deckmonlist:
                 sorteddeckmonlist = list(reversed(deckmonlist))
                 deckmons = []
