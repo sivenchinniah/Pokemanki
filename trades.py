9c52c042a2db9d878d2ba82dea6aaca4e43305c7
Yoshi <zjosua@hotmail.com>
2022-09-23 21:18:58 +0200

    Prevent endless loop when no trades are available

--- trades.py
+++ trades.py
@@ -187,6 +187,7 @@ class Trades:
             elif len(possiblehaves) == 1:
                 have = possiblehaves[0]
             else:
+                i += 1
                 continue
             if have[0].startswith("Eevee") and want[0].startswith("Eevee"):
                 self.trades.append(
@@ -229,6 +230,10 @@ class Trades:
         else:
             self.newTrades()
             tradeData = get_json("_trades.json")
+        if tradeData[1] == []:
+            showInfo(text="No trades are possible at the moment.", parent=mw,
+                     title="Pokémanki")
+            return
         tradewindow = self.tradewindow
         tradewindow.setWindowTitle("Pokemanki")
         tradewindow.setWindowModality(Qt.ApplicationModal)
